5486c847c5c82efa62e29bde9060e7ca5cbd3886
alex <immakakarot@duck.com>
2025-08-24 10:22:13 +0530

    added health indicator

---
+++ src/main/java/com/super30/springbasics/TheBestStudentHealthIndicator.java
@@ -0,0 +1,25 @@
+package com.super30.springbasics;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+
+public class TheBestStudentHealthIndicator implements HealthIndicator {
+
+    private final TheBestStudentRepository repo;
+
+    TheBestStudentHealthIndicator(TheBestStudentRepository repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public Health health() {
+        try {
+            long count = repo.count();
+            return Health.up()
+                    .withDetail("studentCount", count)
+                    .build();
+        } catch (Exception e) {
+            return Health.down(e).build();
+        }
+    }
+}
